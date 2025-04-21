aec221d296c2e25fbb4082cb2a062eaeb1021ecb
Simanto <139176300+SahaSimanto@users.noreply.github.com>
2025-04-22 00:35:44 +0600

    Updated Raw Materials

---
+++ src/main/java/cse213/modernbreakfieldsimulation_simanto_inventoryclerk/DummyTableViewClass.java
@@ -0,0 +1,102 @@
+package cse213.modernbreakfieldsimulation_simanto_inventoryclerk;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
+import java.time.LocalDate;
+
+public class DummyTableViewClass {
+    private int rmPurchasedAmount;
+    private int rmUsedAmount;
+    private int rmRemainingAmount;
+    private int brickProducedAmount;
+    private int brickSoldAmount;
+    private String brickType;
+    private LocalDate rmDop;
+    private LocalDate brickDos;
+
+
+    private static final ObservableList<DummyTableViewClass> items = FXCollections.observableArrayList();
+
+//    public DummyTableViewClass(int rmPurchasedAmount, int rmUsedAmount, LocalDate rmDop) {
+//        this.rmPurchasedAmount = rmPurchasedAmount;
+//        this.rmUsedAmount = rmUsedAmount;
+//        this.rmDop = rmDop;
+//    }
+
+
+    public DummyTableViewClass(int rmPurchasedAmount, int rmUsedAmount, int rmRemainingAmount, int brickProducedAmount, int brickSoldAmount, String brickType, LocalDate rmDop, LocalDate brickDos) {
+        this.rmPurchasedAmount = rmPurchasedAmount;
+        this.rmUsedAmount = rmUsedAmount;
+        this.rmRemainingAmount = rmRemainingAmount;
+        this.brickProducedAmount = brickProducedAmount;
+        this.brickSoldAmount = brickSoldAmount;
+        this.brickType = brickType;
+        this.rmDop = rmDop;
+        this.brickDos = brickDos;
+    }
+
+    public int getRmPurchasedAmount() {
+        return rmPurchasedAmount;
+    }
+
+    public void setRmPurchasedAmount(int rmPurchasedAmount) {
+        this.rmPurchasedAmount = rmPurchasedAmount;
+    }
+
+    public int getRmUsedAmount() {
+        return rmUsedAmount;
+    }
+
+    public void setRmUsedAmount(int rmUsedAmount) {
+        this.rmUsedAmount = rmUsedAmount;
+    }
+
+    public int getRmRemainingAmount() {
+        return rmRemainingAmount;
+    }
+
+    public void setRmRemainingAmount(int rmRemainingAmount) {
+        this.rmRemainingAmount = rmRemainingAmount;
+    }
+
+    public int getBrickProducedAmount() {
+        return brickProducedAmount;
+    }
+
+    public void setBrickProducedAmount(int brickProducedAmount) {
+        this.brickProducedAmount = brickProducedAmount;
+    }
+
+    public int getBrickSoldAmount() {
+        return brickSoldAmount;
+    }
+
+    public void setBrickSoldAmount(int brickSoldAmount) {
+        this.brickSoldAmount = brickSoldAmount;
+    }
+
+    public String getBrickType() {
+        return brickType;
+    }
+
+    public void setBrickType(String brickType) {
+        this.brickType = brickType;
+    }
+
+    public LocalDate getRmDop() {
+        return rmDop;
+    }
+
+    public void setRmDop(LocalDate rmDop) {
+        this.rmDop = rmDop;
+    }
+
+    public LocalDate getBrickDos() {
+        return brickDos;
+    }
+
+    public void setBrickDos(LocalDate brickDos) {
+        this.brickDos = brickDos;
+    }
+}
--- src/main/java/cse213/modernbreakfieldsimulation_simanto_inventoryclerk/RawMaterialsViewController.java
+++ src/main/java/cse213/modernbreakfieldsimulation_simanto_inventoryclerk/RawMaterialsViewController.java
@@ -1,15 +1,19 @@
 package cse213.modernbreakfieldsimulation_simanto_inventoryclerk;
 
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.chart.PieChart;
+import javafx.scene.control.Alert;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 
 
@@ -21,24 +25,24 @@ public class RawMaterialsViewController
 {
 
     @javafx.fxml.FXML
-    private TableView<BrickData> brickDataTableView;
+    private TableView<DummyTableViewClass> brickDataTableView;
     @javafx.fxml.FXML
     private PieChart brickQuantityPieChart;
 
     private ArrayList<BrickData> brickList;
     @FXML
-    private TableColumn<BrickData, LocalDate> rmDopTableColumn;
+    private TableColumn<DummyTableViewClass, LocalDate> rmDopTableColumn;
     @FXML
-    private TableColumn<BrickData, Integer> rmPurchasedAmountTableColumn;
+    private TableColumn<DummyTableViewClass, Integer> rmPurchasedAmountTableColumn;
     @FXML
-    private TableColumn<BrickData, Integer> rmUsedAmountTableColumn;
+    private TableColumn<DummyTableViewClass, Integer> rmUsedAmountTableColumn;
 
     @FXML
     public void initialize() {
         brickList = new ArrayList<BrickData>();
-        rmPurchasedAmountTableColumn.setCellValueFactory(new PropertyValueFactory<BrickData,Integer>("rmPurchasedAmount"));
-        rmUsedAmountTableColumn.setCellValueFactory(new PropertyValueFactory<BrickData,Integer>("rmUsedAmount"));
-        rmDopTableColumn.setCellValueFactory(new PropertyValueFactory<BrickData,LocalDate>("rmDop"));
+        rmPurchasedAmountTableColumn.setCellValueFactory(new PropertyValueFactory<DummyTableViewClass,Integer>("rmPurchasedAmount"));
+        rmUsedAmountTableColumn.setCellValueFactory(new PropertyValueFactory<DummyTableViewClass,Integer>("rmUsedAmount"));
+        rmDopTableColumn.setCellValueFactory(new PropertyValueFactory<DummyTableViewClass,LocalDate>("rmDop"));
 
     }
 
@@ -70,9 +74,79 @@ public class RawMaterialsViewController
 
     @javafx.fxml.FXML
     public void brickQuantityPieChartButtonOnMouseClicked(Event event) {
+        brickQuantityPieChart.getData().clear();
+
+        // Dummy values for now
+        int redBrickAmount = 1300;
+        int clayBrickAmount = 2000;
+
+        ObservableList<PieChart.Data> list = FXCollections.observableArrayList(
+                new PieChart.Data("Red Brick", redBrickAmount),
+                new PieChart.Data("Clay Brick", clayBrickAmount)
+        );
+
+        brickQuantityPieChart.setData(list);
+
+        for (PieChart.Data data : brickQuantityPieChart.getData()) {
+            data.getNode().addEventHandler(MouseEvent.MOUSE_CLICKED, e -> {
+                Alert dataAlert = new Alert(Alert.AlertType.INFORMATION);
+                dataAlert.setHeaderText(null);
+                dataAlert.setContentText(
+                        "Total " + data.getName() + " produced: " + (int) data.getPieValue()
+                );
+                dataAlert.showAndWait();
+            });
+        }
     }
 
     @FXML
     public void loadTableButtonOnMouseClicked(Event event) {
+//        brickDataTableView.getItems().clear();
+//
+//        // For testing, let's populate with dummy data
+//        DummyTableViewClass.getItems().clear(); // Clear previous
+//
+//        for (int i = 0; i < 5; i++) {
+//            int rmPurchasedAmount = 5000 + i * 100;
+//            int rmUsedAmount = 2500 + i * 50;
+//            LocalDate rmDop = LocalDate.now().minusDays(i);
+//
+//            DummyTableViewClass d = new DummyTableViewClass(
+//                    rmPurchasedAmount,
+//                    rmUsedAmount,
+//                    rmDop
+//            );
+//
+//            DummyTableViewClass.getItems().add(d);
+//        }
+//
+//        brickDataTableView.setItems(DummyTableViewClass.getItems());
+//    }
+        brickDataTableView.getItems().clear();
+
+        DummyTableViewClass d1 = new DummyTableViewClass(
+                5000, // rmPurchasedAmount
+                2570, // rmUsedAmount
+                2430, // rmRemainingAmount
+                1300, // brickProducedAmount
+                900,  // brickSoldAmount
+                "Red Brick", // brickType
+                LocalDate.now().minusDays(12), // rmDop
+                LocalDate.now().minusDays(5)   // brickDos
+        );
+
+        DummyTableViewClass d2 = new DummyTableViewClass(
+                4000,
+                3100,
+                900,
+                2000,
+                1500,
+                "Clay Brick",
+                LocalDate.now().minusDays(8),
+                LocalDate.now().minusDays(2)
+        );
+
+        brickDataTableView.getItems().addAll(d1, d2);
     }
+
 }
